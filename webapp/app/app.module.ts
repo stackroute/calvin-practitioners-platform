367f1ee8eb17cd4a3de5f171d6251daa16836c40
admin <adilasanis@gmail.com>
2017-06-13 22:27:59 +0530

    Adding backend code for User  login

--- webapp/app/app.module.ts
+++ webapp/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
@@ -18,7 +18,8 @@ import { LoginComponent} from './login/login.component';
     BrowserAnimationsModule,
     MdButtonModule, MdCheckboxModule,
     FlexLayoutModule, MdSelectModule,
-    MdToolbarModule
+    MdToolbarModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [LoginComponent]
--- webapp/app/login/login.component.ts
+++ webapp/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-
+import { LoginService } from './login-service.component';
 @Component({
   selector: 'calvin-root',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent {
   title = 'app';
   language = [
-    {value: 'E0', viewValue: 'English'},
-    {value: 'E1', viewValue: 'Hindi'},
-    {value: 'E2', viewValue: 'Telugu'}
+    { value: 'E0', viewValue: 'English' },
+    { value: 'E1', viewValue: 'Hindi' },
+    { value: 'E2', viewValue: 'Telugu' }
   ];
+
+  constructor(private loginservice: LoginService) {
+
+  }
+
+  login() {
+
+    alert('login clicked');
+    this.loginservice.loginapp().subscribe(data => {
+      console.log("login doine");
+    })
+
+  }
 }
