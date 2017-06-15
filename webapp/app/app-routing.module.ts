0c4bcfb82be295fddf352cb81f7fe03f659f8613
Nandhini <nandhinisept05@gmail.com>
2017-06-15 13:54:03 +0530

    Added App Bar UI component

--- webapp/app/app-routing.module.ts
+++ webapp/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {UserProfileComponent} from './user-profile/user-profile.component';
 const routes: Routes = [
   {
-    path: '',
+    path: 'profile', component: UserProfileComponent,
     children: []
   }
 ];
--- webapp/app/app.module.ts
+++ webapp/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import{ AppComponent } from './app.component';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { MaterialModule, MdButtonModule, MdCardModule, MdMenuModule } from '@angular/material';
+import { MdToolbarModule, MdIconModule, MdInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+import { AppbarComponent } from './appbar/appbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+   AppComponent,
+    UserProfileComponent,
+   
+    AppbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
