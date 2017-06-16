44ea6fd214656d31b8ece9a4b62b69932d59c042
nikitaraturi12 <niki.raturi@gmail.com>
2017-06-16 20:52:11 +0530

    made changes in the UI of manage community component

--- webapp/app/core/app-routing.module.ts
+++ webapp/app/core/app-routing.module.ts
@@ -6,7 +6,7 @@ import { UserDashboardComponent } from '../user-dashboard/user-dashboard.compone
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { CanDeactivateGuard } from './can-deactivate-guard.service';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
-import { ManageCommunityComponent } from "../manageCommunity/manage-community.component";
+import { ManageCommunityComponent } from '../manageCommunity/manage-community.component';
 import { AppBarComponent } from '../app-bar/app-bar.component';
 import { RoleManagementComponent } from '../role-management/role-management.component';
 import { AuthGuard } from './auth-guard.service';
@@ -23,7 +23,6 @@ const routes: ModuleWithProviders = RouterModule.forRoot([
             { path: 'profile', component: UserProfileComponent },
             { path: 'role', component: RoleManagementComponent},
             { path: 'managecommunity', component: ManageCommunityComponent}
-            
         ]
   },
   { path: '', redirectTo: '/app/home', pathMatch: 'full' },
