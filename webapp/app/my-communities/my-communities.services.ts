9ce5bc38b2cda50061fb5f0bee381be23f33fb3d
admin <adilasanis@gmail.com>
2017-06-19 14:34:23 +0530

    Fixed FLow issue in Login Functionality

--- webapp/app/my-communities/my-communities.services.ts
+++ webapp/app/my-communities/my-communities.services.ts
@@ -1,14 +1,11 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-
 @Injectable()
-export class mycommunities {
-
-  constructor (private http:Http){ }
-   
-  getCommunity(){
-    const url='';
+export class MyCommunities {
+  constructor (private http:Http){ } 
+getCommunity(){
+  const url='';
     return this.http.get(url).map((response:Response) => response.json());
   }
 
