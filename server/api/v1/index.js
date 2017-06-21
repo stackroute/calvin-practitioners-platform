b3de1dfe04d95bbcdc487268bc52f57cefe56981
renuka-nagaraj <renukanrj@gmail.com>
2017-06-21 20:15:50 +0530

    json error fixed

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -6,6 +6,9 @@ router.use('/logout', (req, res) => {
   res.clearCookie('currentUser');
   res.redirect('/');
 });
+router.use('/members', require('./modules/members'));
+
+router.use('/tools', require('./modules/tools'));
 
 router.use(require('./modules/authentication'));
 
@@ -16,9 +19,7 @@ router.use('/userCommunities', require('./modules/community'));
 router.use('/community', require('./modules/community'));
 
 
-router.use('/members', require('./modules/members'));
 
-router.use('/tools', require('./modules/tools'));
 
 
 router.use('/community-role-action', require('./modules/community-role-action'));
