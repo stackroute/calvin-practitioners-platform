e0448c386c590001841a986e4e185ca3d66b10be
nikitaraturi12 <niki.raturi@gmail.com>
2017-06-22 09:17:45 +0530

    created api for getting community details

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-router.use('/manage-community', require('./modules/community'));
+router.use('/community-details', require('./modules/community-details'));
 // middleware to validate token
 router.use((req, res, next) => {
 //   console.log('inside middle ware');
   const url = req.url;
 //   console.log('url', url);
-if (url.includes('google') || url.includes('logout')||('community')) {
-    next();
-  } else {
-    const user = req.cookies.currentUser;
-    if (user) {
+if (url.includes('google') || url.includes('logout') || ('community')) {
+next();
+} else {
+const user = req.cookies.currentUser;
+if (user) {
       jwt.verify(user, 'secret key', (err) => {
         if (err) {
         //   console.log('error', err);
@@ -22,7 +22,7 @@ if (url.includes('google') || url.includes('logout')||('community')) {
     //   console.log('cookie is :', user);
       next();
     } else { res.redirect('/'); }
-  }
+   }
 });
 router.use('/members', require('./modules/members'));
 
@@ -46,8 +46,6 @@ router.use(require('./modules/authentication'));
 router.use('/activity', require('./modules/activity-page'));
 
 router.use('/userCommunities', require('./modules/community'));
-router.use('/community', require('./modules/community'));
+// router.use('/community-details', require('./modules/community-details'));
 router.use('/community-role-action', require('./modules/community-role-action'));
-
-
 module.exports = router;
