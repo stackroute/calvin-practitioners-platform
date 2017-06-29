db7fbaec6bb02868dec9b434fe666d326115517f
programoholic <adilasanis@gmail.com>
2017-06-29 18:22:19 +0530

    Added Muliple action feature while adding Tool

--- webapp/app/add-tool/add-tool.component.ts
+++ webapp/app/add-tool/add-tool.component.ts
@@ -7,16 +7,50 @@ import { NgForm } from '@angular/forms';
 })
 export class AddToolComponent implements OnInit {
 
+
+  actions = [];
+  events = [];
+  toolaction: String;
+  toolgrants: String;
+  actionFlag = 0;
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+
+  focusOutFunction() {
+
+    //  alert('hi'+this.toolaction);  
+    let obj = {
+      name: this.toolaction,
+      grants: this.toolgrants
+    };
+    this.actions.push(obj);
+    this.actionFlag = 1;
+  }
+
+  enableAction() {
+    this.toolaction = '';
+    this.toolgrants = '';
+    this.actionFlag = 0;
+  }
+
+  remAction() {
+    alert(this.toolaction);
+     this.actions = this.actions.filter(item => item.name !== this.toolaction);
+    console.log(this.actions);
   }
 
   registerTool(form: NgForm) {
     alert('called');
     console.log(form.value);
-    // {email: '...', password: '...'}
+       
+      //  
+     // {email: '...', password: '...'}
     // ... <-- now use JSON.stringify() to convert form values to json.
-  }
+   //...  <-- now
 
+ }
 }
