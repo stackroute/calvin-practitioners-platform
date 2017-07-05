9e5add71727dda4128d750a441f1364779664020
saranya-babu <saransri95@gmail.com>
2017-07-05 09:45:52 +0530

    modified routing with param in dialogbox component

--- webapp/app/newcommunity-dialogbox/newcommunity-dialogbox.component.ts
+++ webapp/app/newcommunity-dialogbox/newcommunity-dialogbox.component.ts
@@ -15,9 +15,9 @@ export class NewcommunityDialogboxComponent implements OnInit {
 constructor(private router: Router) { }
 
 routeToManage() {
-    this.router.navigate(['/app/managecommunity']);
+    this.router.navigate(['/app/managecommunity/']);
   }
 
-ngOnInit() { }
+ngOnInit() {}
 
 }
