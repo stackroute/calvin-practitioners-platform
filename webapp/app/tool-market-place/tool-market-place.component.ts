1564b3fb3df40f02cb53ac1e0c8db1c54c083d8f
renuka-nagaraj <renukanrj@gmail.com>
2017-07-12 20:10:09 +0530

    filteration for toolmarket place based on toolname and purpose

--- webapp/app/tool-market-place/tool-market-place.component.ts
+++ webapp/app/tool-market-place/tool-market-place.component.ts
@@ -10,23 +10,47 @@ import { ToolMarketService } from './tool-market-place.service';
 export class ToolMarketPlaceComponent implements OnInit {
 
   tools;
+  newTool;
   constructor(private toolService: ToolMarketService) { }
 
   ngOnInit() {
     this.toolService.getTools().subscribe(data => {
       this.tools = data;
+      this.newTool=data;
       console.log('this.tools', this.tools);
+      
     });
   }
 
-  // let searchValue;
+  //searchValue;
   selectedValue(value1) {
+    
+    this.newTool=[];
     this.tools.forEach((data) => {
-      if (value1 === data.toolname) {
-        console.log("true", data);
-
+       console.log('this toolname',data.toolname);
+        console.log('this toolname type',typeof data.toolname);
+      if (data.toolname.includes(value1.toUpperCase())) {
+       
+       console.log("true", data);
+       this.newTool.push(data);
+       
       }
     });
+  }
+
+  selectpurpose(value){
+    console.log(value);
+    //value="test";
+    this.newTool=[];
+    this.tools.forEach((data) => {
+      console.log('this toolname',data.purpose);
+      if (data.purpose===value) {
+       
+       console.log("purpose true", data);
+       this.newTool.push(data);
+      }
+    });
+    
   }
   
 }
