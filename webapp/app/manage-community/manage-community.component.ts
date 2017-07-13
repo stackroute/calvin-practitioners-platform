9c74bc2d6381f5db8f13b092bf34570bdc618ec8
nitin.geeks <godfather392@gmail.com>
2017-07-13 21:11:36 +0530

    code refactoring and invite member

--- webapp/app/manage-community/manage-community.component.ts
+++ webapp/app/manage-community/manage-community.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Params, RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
 import { MdDialog } from '@angular/material';
-import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 1
 import * as moment from 'moment/moment';
 
@@ -18,89 +18,89 @@ import { updateSpecificCommunityService } from '../manage-community/manage-commu
   templateUrl: './manage-community.component.html',
   styleUrls: ['./manage-community.component.css'],
   providers: [CommunityProfileService, updateSpecificCommunityService],
-  
+
 })
 export class ManageCommunityComponent implements OnInit {
 
 
-    userForm: FormGroup;
+  userForm: FormGroup;
 
-community = [];
-selectedValue: string;
-public tagarray= [];
-url: string;
-param = [];
-domain;
-// myAvatar;
-updatedBy;
-status;
-counter = true;
-ans;
-tagname:String;
-  
-   constructor(private route: ActivatedRoute, private router: Router,
-   private commProfileService: CommunityProfileService,private dialog: MdDialog, private commUpdateService: updateSpecificCommunityService) { }
+  community = [];
+  selectedValue: string;
+  public tagarray = [];
+  url: string;
+  param = [];
+  domain;
+  // myAvatar;
+  updatedBy;
+  status;
+  counter = true;
+  ans;
+  tagname: String;
 
-   chipValue(tag) {
+  constructor(private route: ActivatedRoute, private router: Router,
+    private commProfileService: CommunityProfileService, private dialog: MdDialog, private commUpdateService: updateSpecificCommunityService) { }
 
-  //  alert("getting called chip");
-   this.tagarray.push(tag);
-  //  this.tagname = '';
+  chipValue(tag) {
+
+    //  alert("getting called chip");
+    this.tagarray.push(tag);
+    //  this.tagname = '';
 
 
-   
- } 
+
+  }
   // deselect chip value/remove tag value from an array
   clearTag(tag) {
-  const tagvalue = tag;
-  this.tagarray = this.tagarray.filter(item => item !== tagvalue);
-  
- }
-openDialog() {
+    const tagvalue = tag;
+    this.tagarray = this.tagarray.filter(item => item !== tagvalue);
+
+  }
+  openDialog() {
     this.dialog.open(updateCommunity);
   }
 
 
- onSubmit(Form: any,): void {  
-  //  if (this.status == 'active') {
-  //    this.status = 'enable';
-  //    console.log('testing----------',this.status);
-  //     return this.status;
-  //  } else {
-  //         this.status = 'disable';
-  //    console.log('testing----------',this.status);
+  onSubmit(Form: any, ): void {
+    //  if (this.status == 'active') {
+    //    this.status = 'enable';
+    //    console.log('testing----------',this.status);
+    //     return this.status;
+    //  } else {
+    //         this.status = 'disable';
+    //    console.log('testing----------',this.status);
 
-  //          return this.status;
-  //         }
+    //          return this.status;
+    //         }
     Form.updatedby = this.updatedBy;
     Form.tags = this.tagarray;
-    
-   
-    this.commUpdateService.updateSpecificCommunity(Form,this.domain ).subscribe(
-    (data) => console.log('posted data',Form, this.domain),
-    () => console.log('finished'))
+
+
+    this.commUpdateService.updateSpecificCommunity(Form, this.domain).subscribe(
+      (data) => console.log('posted data', Form, this.domain),
+      () => console.log('finished'))
     this.openDialog();
-}
- 
- 
+  }
 
 
- ngOnInit() {
-   
-   this.domain = this.route.snapshot.params['domain'];
-   this.commProfileService.getCommunity(this.route.snapshot.params['domain']). subscribe ( res => {   
-   this.domain = res.domain;
-   this.updatedBy = res.updatedby;
-   res.createdon= moment(res.createdon).subtract(1,'days').calendar();
-   this.community = res;
-   console.log("inside managecommunity",this.community);
 
- 
-  
-  //  this.tagarray.push(res.tags);
-  } );
 
-}
+  ngOnInit() {
+
+    this.domain = this.route.snapshot.params['domain'];
+    this.commProfileService.getCommunity(this.route.snapshot.params['domain']).subscribe(res => {
+      this.domain = res.domain;
+      this.updatedBy = res.updatedby;
+      res.createdon = moment(res.createdon).subtract(1, 'days').calendar();
+      this.community = res;
+      console.log("inside managecommunity", this.community);
+
+
+
+      //  this.tagarray.push(res.tags);
+    });
+
+  }
 }
 
 @Component({
@@ -110,6 +110,6 @@ openDialog() {
 export class updateCommunity {
 
   constructor(private router: Router) { }
- 
+
 }
 
