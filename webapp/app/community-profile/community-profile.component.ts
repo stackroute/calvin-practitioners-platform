ff61ac5dcad59428ae5de5166bf0fa0dfac5c948
nikitaraturi12 <niki.raturi@gmail.com>
2017-07-14 18:20:06 +0530

    added avatar in user communities list

--- webapp/app/community-profile/community-profile.component.ts
+++ webapp/app/community-profile/community-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import * as moment from 'moment/moment';
 import { UserInfoService } from '../core/user-info.service';
 
--- webapp/app/my-communities/my-communities.services.ts
+++ webapp/app/my-communities/my-communities.services.ts
@@ -11,18 +11,18 @@ export class UserCommunities {
  
  constructor (private http: Http,public snackBar: MdSnackBar) { }
 //  member = "mr.w" ;
-    getCommunity(member) {
-    const url = `/api/v1/communities/membership/${member}`;
-    return this.http.get(url).catch(err => {
-    this.snackBar.open('Unable to get Communities.. Please try again later..!!!','Close',{
-        duration: 3000
-      });
-      return Observable.throw(err); // observable needs to be returned or exception raised
-    })
-    .map( response => response.json());
+    // getCommunity(member) {
+    // const url = `/api/v1/communities/membership/${member}`;
+    // return this.http.get(url).catch(err => {
+    // this.snackBar.open('Unable to get Communities.. Please try again later..!!!','Close',{
+    //     duration: 3000
+    //   });
+    //   return Observable.throw(err); // observable needs to be returned or exception raised
+    // })
+    // .map( response => response.json());
   
 
-  }
+
 
 }
   
