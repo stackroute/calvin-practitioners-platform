07045d38301705fd9985e0c7e0b19768a216b651
programoholic <adilasanis@gmail.com>
2017-07-15 08:06:51 +0530

    Added Methods  in Webhook module

--- server/api/v1/modules/webhooks/webhook.controller.js
+++ server/api/v1/modules/webhooks/webhook.controller.js
@@ -1,39 +1,54 @@
 const async = require('async');
 const extractTool = require('url');
-function verifyToken(token, done) {
+const jwt = require('jsonwebtoken');
+const config = require('../common/config');
+const path = require('path');
+const communityController = require('../../modules/communitytools/communitytools.controller');
 
-}
-
-function extractEventData(data, done) {
-
-}
-
-function sendToCommunityService(data, done) {
-
-    extractTool.method(data, (err, result) => {
-
+function verifyToolToken(token, done) {
+    jwt.verify(token, config.appConstants.secret, (err, tokenClaims) => {
+        if (err) {
+            return done(err);
+        }
+        done(null, tokenClaims);
     });
 }
 
-function sendToolDataToCommunity(token, done) {
+function extractEventData(eventPayload, tokenClaims, done) {
+    let toolPath = path.resolve(__dirname, '../', 'toolproxies', tokenClaims.toolId);
+    const toolservice = require(toolPath);
+    toolPath.extractEventData(tokenClaims, (err, extractedData) => {
+        if (err) {
+            return done(err, 'unable to extract data');
+        }
+        done(null, extractedData);
+    });
+}
 
+function sendToCommunityService(eventPayload, extractedData, done) {
+    communityController.postTool(extractedData.domain, extractedData,(err,result)=>{
+            
+            if(err){
+                return done(err,'Unable to POST Tool in Community');
+            }
+        return done(null,'Successfully Sent');
+    });
+}
+
+function handleToolEvent(token, eventPayload, done) {
     async.waterfall([
-        verifyToken,
-        extractEventData,
-        sendToCommunityService
+        verifyToolToken.bind(null, token),
+        extractEventData.bind(null, eventPayload),
+        sendToCommunityService.bind(null, eventPayload)
     ],
-        (req, res) => {
-
+        (err, result) => {
             if (err) {
-
                 done(err, 'Internal Error');
             }
             done(null, 'successfully sent');
         });
-
-
 }
 
 module.exports = {
-    sendToolDataToCommunity
+    handleToolEvent
 }
