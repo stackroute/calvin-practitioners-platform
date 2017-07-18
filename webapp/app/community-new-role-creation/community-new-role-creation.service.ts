1a34161d7522d7b605140abe57e68383c66ddd6d
GeethaNehru <geetha8dec@gmail.com>
2017-07-18 08:22:21 +0530

    added tool actions without page refreshing

--- webapp/app/community-new-role-creation/community-new-role-creation.service.ts
+++ webapp/app/community-new-role-creation/community-new-role-creation.service.ts
@@ -4,26 +4,21 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { MdSnackBar} from '@angular/material';
 import 'rxjs/Rx';
-
 @Injectable()
 export class NewRoleService {
-  constructor(private http: Http,private snackBar:MdSnackBar) { }
+ constructor(private http: Http, private snackBar: MdSnackBar) {}
 
-  listTools(domainName) {
-    //console.log('In service');
-    const url = '/api/v1/communitytools/'+domainName;
-    //console.log(url)
-    return this.http.get(url).catch(err => {
-                this.snackBar.open('server error..!!!', 'please try again later!', {
-                    duration: 3000
-                });
-                return Observable.throw(err); // observable needs to be returned or exception raised
-            }).map(res => res.json());
-  }
+ listTools(domainName) {
+  const url = '/api/v1/communitytools/' + domainName;
+  return this.http.get(url).catch(err => {
+   this.snackBar.open('server error..!!!', 'please try again later!', {
+    duration: 3000
+   });
+   return Observable.throw(err); // observable needs to be returned or exception raised
+  }).map(res => res.json());
+ }
 
-updateTools(domainName,data) {
-  // console.log(domainName);
-  // console.log(data);  
+ updateTools(domainName, data) {
   const headers = new Headers({
    'Content-Type': 'application/json;charset=utf-8'
   });
@@ -32,23 +27,21 @@ updateTools(domainName,data) {
   });
   const body = JSON.stringify(data);
   //console.log(body);
-  const url = '/api/v1/communityroleactions/communityrole/'+domainName;
-  return this.http.post(url, body,options).catch(err => {
-                this.snackBar.open('server error..!!!', 'please try again later!', {
-                    duration: 3000
-                });
-                return Observable.throw(err); // observable needs to be returned or exception raised
-            }).map(res => res.json());
+  const url = '/api/v1/communityroleactions/communityrole/' + domainName;
+  return this.http.post(url, body, options).catch(err => {
+   this.snackBar.open('server error..!!!', 'please try again later!', {
+    duration: 3000
+   });
+   return Observable.throw(err); // observable needs to be returned or exception raised
+  }).map(res => res.json());
  }
-   listUniqueRoles(domainname) {
-    //console.log('In service');
-    const url = '/api/v1/communityroleactions/' + domainname + '?onlyroles=true';
-    // console.log(url)
-    return this.http.get(url).catch(err => {
-                this.snackBar.open('server error..!!!', 'try again!', {
-                    duration: 3000
-                });
-                return Observable.throw(err); // observable needs to be returned or exception raised
-            }).map(res => res.json());
-  }
+ listUniqueRoles(domainname) {
+  const url = '/api/v1/communityroleactions/' + domainname + '?onlyroles=true';
+  return this.http.get(url).catch(err => {
+   this.snackBar.open('server error..!!!', 'try again!', {
+    duration: 3000
+   });
+   return Observable.throw(err); // observable needs to be returned or exception raised
+  }).map(res => res.json());
+ }
 }
--- webapp/app/community-tool-actions/community-tool-actions.component.ts
+++ webapp/app/community-tool-actions/community-tool-actions.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit,Input, Inject, Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit, Input, Inject, Pipe, PipeTransform } from '@angular/core';
 import { ToolActions } from './community-tool-actions.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Params, RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
-import { MD_DIALOG_DATA, MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
+// import { Params, RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
+import { MD_DIALOG_DATA, MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
 import { KeysPipe } from './community-tool-actions.pipe';
-import { CommunityRoleManagementComponent } from '../community-role-management/community-role-management.component';
+// import { CommunityRoleManagementComponent } from '../community-role-management/community-role-management.component';
 import { RoleServices } from '../community-role-management/community-role-management.service';
 
 
@@ -12,86 +12,87 @@ import { RoleServices } from '../community-role-management/community-role-manage
   selector: 'calvin-community-tool-actions',
   templateUrl: './community-tool-actions.component.html',
   styleUrls: ['./community-tool-actions.component.css'],
-  providers:[CommunityRoleManagementComponent,RoleServices]
+  providers: [RoleServices]
 })
 export class CommunityToolActionsComponent implements OnInit {
-  selected = [];
-  getresults=[];
-  
-  checkBoxValue: boolean = false;
-  sample = [];
-  a=[];
-  domainName;
-  roleName;
-  toolActions=[];
-  y={};
-  @Input() community;
- 
-  constructor(private tool: ToolActions,private role: RoleServices, private snackBar:MdSnackBar, private route:ActivatedRoute,@Inject(MD_DIALOG_DATA) public data: any,
-  public dialogRef: MdDialogRef<CommunityToolActionsComponent>,private roleManage:CommunityRoleManagementComponent) { 
-  this.domainName = data.domain ; 
-  this.roleName=data.role;
-  this.toolActions=data.tool;
-  console.log(data.domain);
-  console.log('domain name from dialog',this.roleName);  
-  console.log('tool',this.toolActions); 
-      
-}
+ selected = [];
+ getresults = [];
 
-  getCheckboxValue(toolId, status) {
-    // console.log(id);
-    //console.log(role);
-    const grant = "true";
-    //console.log(status)
-    const x = {};
-    x[status] = grant;
+ checkBoxValue: boolean = false;
+ sample = [];
+ toolArray = [];
+ domainName;
+ roleName;
+ toolActions = [];
+ y = {};
+ @Input() community;
 
-   // console.log(x)
-    // console.log(action);
-    const actions = x;
-    const deleted = this.selected
-   const index = this.selected.indexOf({toolId,actions});
-  
-   // console.log({toolId,actions,role});
-    //console.log(this.selected.indexOf({toolId,actions,role}));
-    if (index === -1) {
-      this.selected.push({toolId,actions});
-    }
-    if(index > -1) {
-      console.log(index)
-      this.selected.splice(index,1);
-    }
-    console.log(this.selected, "fdsgsdgsdg");
-    return this.selected;
-
-  }
-  
-  exists(toolId, status) {
-    console.log(this.selected);
-    return this.selected.indexOf({toolId,status}) > -1;
-  }
-  update()
-  {
-    console.log(this.selected);
-    console.log(this.domainName);
-    console.log(this.roleName);
-    return this.tool.updateTools(this.domainName,this.roleName,this.selected).subscribe(res=>{
-      this.dialogRef.close('close');
-      this.snackBar.open('Updated Actions Successfully','X',{
-        duration:2000
-      });
-      this.sample.push(res);
-      console.log(this.sample);
-      //this.roleManage.roleManagement();
-    });
-    
-  }
-
-  ngOnInit() {
-    this.tool.listTools(this.domainName).subscribe(res => {return this.sample.push(res);
-    });
-   
-  }
- 
+ constructor(private tool: ToolActions,
+  private role: RoleServices,
+  private snackBar: MdSnackBar,
+  @Inject(MD_DIALOG_DATA) public data: any,
+  public dialogRef: MdDialogRef < CommunityToolActionsComponent > ) {
+  this.domainName = data.domain;
+  this.roleName = data.role;
+  this.toolActions = data.tool;
  }
- 
+
+ getCheckboxValue(toolId, status) {
+  const grant = "true";
+  const x = {};
+  x[status] = grant;
+  const actions = x;
+  const deleted = this.selected
+  const index = this.selected.indexOf({
+   toolId,
+   actions
+  });
+  if (index === -1) {
+   this.selected.push({
+    toolId,
+    actions
+   });
+  }
+  if (index > -1) {
+   this.selected.splice(index, 1);
+  }
+  return this.selected;
+
+ }
+
+ exists(toolId, status) {
+  console.log(this.selected);
+  return this.selected.indexOf({
+   toolId,
+   status
+  }) > -1;
+ }
+
+ update() {
+  this.tool.updateTools(this.domainName, this.roleName, this.selected)
+   .subscribe(res => {
+    this.snackBar.open('Updated Actions Successfully', 'X', {
+     duration: 2000
+    });
+    this.sample=[];
+    this.toolArray.push(res);
+    this.sample = this.toolArray;
+    this.dialogRef.close({
+     roleName: this.roleName,
+     selected: this.selected,
+     res: res
+    });
+   });
+    this.sample=[];
+    
+ }
+
+ ngOnInit() { 
+   
+  this.tool.listTools(this.domainName).subscribe(res => {
+   this.sample.push(res);
+  });
+  
+ }
+
+}
