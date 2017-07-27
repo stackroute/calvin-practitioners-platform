760b57c37beee436b01b1158f8c8dd7106cf5072
nitin.geeks <godfather392@gmail.com>
2017-07-27 15:04:40 +0530

    add db file

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const clientPath = path.resolve(__dirname, '..', 'dist');
 app.use(express.static(clientPath));
 
 app.use('/api/v1', require('./api/v1'));
-
+require('./dbsetup.js');
 app.use((req, res) => {
   res.status(404).send({ error: 'resource not found' });
 });
